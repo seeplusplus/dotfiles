938edaeafe71c78e627438a6d19d7c166154bc38
Caleb Webber <caleb@codingthemsoftly.com>
2023-05-05 08:28:35 -0400

    add lsp, rls, maps

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,6 +24,29 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
+
+	use 'folke/tokyonight.nvim'
+	use {
+	    'VonHeikemen/lsp-zero.nvim',
+	    branch = 'v2.x',
+	    requires = {
+	      -- LSP Support
+	      {'neovim/nvim-lspconfig'},             -- Required
+	      {                                      -- Optional
+		'williamboman/mason.nvim',
+		build = function()
+		  pcall(vim.cmd, 'MasonUpdate')
+		end,
+	      },
+	      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	      -- Autocompletion
+	      -- {'hrsh7th/nvim-cmp'},     -- Required
+	      -- {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	      -- {'L3MON4D3/LuaSnip'},     -- Required
+	    }
+	  }
+
 	if packer_bootstrap then
 		require('packer').sync()
 	end
