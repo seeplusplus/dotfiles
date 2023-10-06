d1c81070e1bfb2035c5c9696a759f0a22b163b76
Caleb Webber <caleb@codingthemsoftly.com>
2023-10-05 23:31:38 -0400

    configure ultisnips and nvim cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,3 +14,18 @@ require('plugins')
 require('set')
 require('tokyo')
 require('lsp')
+
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+  expand = function(args)
+      vim.fn['UltiSnips#Anon'](args.body)
+    end
+  },
+  sources = cmp.config.sources({
+  { name = 'nvim_lsp' },
+    { name = 'ultisnips' }
+  }, {
+    { name = 'buffer' }
+  })
+}
