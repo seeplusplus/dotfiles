d2b541c939058453394820964c94445dea80e49d
Caleb Webber <caleb@codingthemsoftly.com>
2023-10-15 11:19:56 -0400

    plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,3 +29,12 @@ cmp.setup {
     { name = 'buffer' }
   })
 }
+
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "rust", "elixir" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true
+  }
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,3 @@
--- Only required if you have packer configured as `opt'
--- `vim.cmd.packadd('packer.nvim')
-
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -15,40 +12,36 @@ end
 ensure_packer()
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-
-	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} },
-	}
-
-
+  use 'wbthomason/packer.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} },
+  }
+  use { 
+    "gennaro-tedesco/nvim-jqx",
+    ft = { "json", "yaml" },
+  }
   use 'SirVer/ultisnips'
   use 'folke/tokyonight.nvim'
   use 'terrastruct/d2-vim'
-	use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v3.x',
-      -- LSP Support
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-      {'neovim/nvim-lspconfig'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-
-	  }
-
-  -- use "sindrets/diffview.nvim"
+  use { "elixir-tools/elixir-tools.nvim", tag = "stable", requires = { "nvim-lua/plenary.nvim" }}
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lua'},
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+  }
+  use "adelarsq/neofsharp.vim"
   use "rhysd/conflict-marker.vim"
 	if packer_bootstrap then
 		require('packer').sync()
